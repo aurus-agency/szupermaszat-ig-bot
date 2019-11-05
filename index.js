5b178054c794f72eb18ea632087cbcc814b971c6
Márk <pap.mark@aurus.agency>
2019-11-05 20:06:49 +0100

    :bug: Fixed follow bug

--- index.js
+++ index.js
@@ -402,11 +402,12 @@ const unfollowFollowedUsers = async () => {
     try {
       console.log('Sending unfollow to: ' + dbUsers[0].fullName + ',' + dbUsers[0].username);
       await ig.entity.profile(dbUsers[0].id).checkUnfollow();
-      await updateDocument(dbUsers[0]._id, {
+      /* await updateDocument(dbUsers[0]._id, {
         closed: true,
         timestamp: now,
-      }, 'sonar');
+      }, 'sonar'); */
     } catch (e) {
+      console.log(e);
       throw new Error('There was an error sending unfollow to user: ' + dbUsers[0].name)
     }
   } else {
